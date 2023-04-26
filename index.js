9c2ebdaba42aff0d2725079dd024084447cbb666
keniaDE <decoteaukenia@gmail.com>
2023-04-26 11:27:35 -0400

    forgot to save index.js

--- index.js
+++ index.js
@@ -2,3 +2,6 @@ console.log(repo)
 /// git ststus
 // git init
 //rm -rf .git will remove hidden .git file
+//the period in git add . means the current directory and all files within it, 
+//recursively. (The asterisk in git add * , on the other hand, would mean all files in the
+// current directory except ones that begin with a period.)
